9c2c415d1e78b58f3fceda362b7ad112df062ecb
Nikhil <84652874+nikhil2382@users.noreply.github.com>
2024-12-08 16:49:34 +0530

    Actions Type Changes

--- src/utils/types/actions.types.ts
+++ src/utils/types/actions.types.ts
@@ -1,12 +1,14 @@
 import { TYPES } from 'actions/types';
+import { ActionsInputTypes } from './actionInputs.types';
 
-export type Action<T, D = undefined> = D extends undefined
-  ? { type: T }
-  : { type: T; data: D };
+export type Action<T extends keyof ActionsInputTypes> =
+  T extends keyof ActionsInputTypes
+    ? { type: T }
+    : { type: T; data: ActionsInputTypes[T] };
 
 export type LogoutAction = Action<typeof TYPES.GENERAL.LOGOUT>;
 
-export type HomeStartAction = Action<typeof TYPES.HOME.START, string>;
+export type HomeStartAction = Action<typeof TYPES.HOME.START>;
 export type HomeSuccessAction = Action<typeof TYPES.HOME.SUCCESS>;
 export type HomeFailureAction = Action<typeof TYPES.HOME.FAILURE>;
 
